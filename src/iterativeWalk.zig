18a84dbe955e89c44a121d08d80ba0bc5d91f77c
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-06-10 19:10:40 +0200

    Iterative way now without recursion

--- src/iterativeWalk.zig
+++ src/iterativeWalk.zig
@@ -24,47 +24,44 @@ pub const IterativeWalker = struct {
     }
 
     pub fn next(self: *Self) anyerror!?Entry {
-        if (try self.currentDir.next()) |entry| {
-            var full_entry_buf = std.ArrayList(u8).init(self.allocator);
-            try full_entry_buf.resize(self.currentPath.len + entry.name.len + 1);
-     
-            const full_entry_path = full_entry_buf.toSlice();
-            std.mem.copy(u8, full_entry_path, self.currentPath);
-            full_entry_path[self.currentPath.len] = std.fs.path.sep;
-            std.mem.copy(u8, full_entry_path[self.currentPath.len + 1 ..], entry.name);
-
-            if (entry.kind == std.fs.Dir.Entry.Kind.Directory) {
-                const new_dir = try self.allocator.create(std.atomic.Queue([]u8).Node);
-                new_dir.* = std.atomic.Queue([]u8).Node {
-                    .next = undefined,
-                    .prev = undefined,
-                    .data = full_entry_path,
-                };
-
-                self.pathsToScan.put(new_dir);
-            }
-
-            return Entry{
-                .name = entry.name,
-                .absolutePath = full_entry_path,
-                .kind = entry.kind,
-            };
-        } else {
-            // No entries left in the current dir
-            self.currentDir.close();
-            while (self.pathsToScan.get()) |node| {
-                self.currentPath = node.data;
-                self.currentDir = try std.fs.Dir.open(self.allocator, self.currentPath);
-
-                self.allocator.destroy(node);
-
-                if (try self.next()) |entry| {
-                    return entry;
-                } else {
-                    continue;
+        outer: while (true) {
+            if (try self.currentDir.next()) |entry| {
+                var full_entry_buf = std.ArrayList(u8).init(self.allocator);
+                try full_entry_buf.resize(self.currentPath.len + entry.name.len + 1);
+         
+                const full_entry_path = full_entry_buf.toSlice();
+                std.mem.copy(u8, full_entry_path, self.currentPath);
+                full_entry_path[self.currentPath.len] = std.fs.path.sep;
+                std.mem.copy(u8, full_entry_path[self.currentPath.len + 1 ..], entry.name);
+    
+                if (entry.kind == std.fs.Dir.Entry.Kind.Directory) {
+                    const new_dir = try self.allocator.create(std.atomic.Queue([]u8).Node);
+                    new_dir.* = std.atomic.Queue([]u8).Node {
+                        .next = undefined,
+                        .prev = undefined,
+                        .data = full_entry_path,
+                    };
+    
+                    self.pathsToScan.put(new_dir);
                 }
+    
+                return Entry{
+                    .name = entry.name,
+                    .absolutePath = full_entry_path,
+                    .kind = entry.kind,
+                };
+            } else {
+                // No entries left in the current dir
+                self.currentDir.close();
+                if (self.pathsToScan.get()) |node| {
+                    self.currentPath = node.data;
+                    self.currentDir = try std.fs.Dir.open(self.allocator, self.currentPath);
+                    self.allocator.destroy(node);
+
+                    continue :outer;
+                }
+                return null;
             }
-            return null;
         }
     }
 };
