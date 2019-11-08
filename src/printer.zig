9c1bd7bf03d70441c19d6f88159e02088c3eee77
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-11-08 16:47:58 +0100

    Update

--- src/printer.zig
+++ src/printer.zig
@@ -3,10 +3,10 @@ const std = @import("std");
 const Entry = @import("zig-walkdir/src/entry.zig").Entry;
 
 pub fn printEntryFile(entry: Entry, out: std.fs.File) !void {
-    try out.write(entry.relativePath);
+    try out.write(entry.relative_path);
     try out.write("\n");
 }
 
 pub fn printEntryStream(entry: Entry, out: var) !void {
-    try out.print("{}\n", entry.relativePath);
+    try out.print("{}\n", entry.relative_path);
 }
